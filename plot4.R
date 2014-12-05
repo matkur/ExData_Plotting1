750e48828b93057071da72eecb557022fe827ef7
matkur <matti.x.kurki@hotmail.fi>
2014-12-05 15:56:31 +0200

    V4

--- plot4.R
+++ plot4.R
@@ -12,16 +12,29 @@ data2["DateTime"]<- as.POSIXct(data2$DateTime, format='%d/%m/%Y %H:%M')
 data1<-NULL
 
 png(
-    "plot3.png",
+    "plot4.png",
     width = 480,
     height = 480,
     units = "px",
 )
 
+par(mfrow=c(2,2))
+
+#Upper left
+plot(data2$DateTime,data2$Global_active_power,ylab='Global Active Power (kilowatts)', xlab='', type='l')
+
+#upper right
+plot(data2$DateTime,data2$Voltage,xlab='datetime',ylab='Voltage',type='l')
+
+#bottom left
 plot(data2$DateTime,data2$Sub_metering_1,ylab='Energy sub metering', xlab='', type='l',col='black', lty="solid")
 lines(data2$DateTime,data2$Sub_metering_2,col='red', lty="solid")
 lines(data2$DateTime,data2$Sub_metering_3,col='blue', lty="solid")
-
 legend('topright',c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=c('solid','solid','solid'))
- 
+
+#bottom right
+plot(data2$DateTime,data2$Global_reactive_power,xlab='datetime',ylab='Global_reactive_power',type='l')
+
+
+
 dev.off()

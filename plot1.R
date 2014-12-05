ccbfcdb72a651b9ae3c9d65423b0b91782b97453
matkur <matti.x.kurki@hotmail.fi>
2014-12-05 16:39:57 +0200

    Final

--- plot1.R
+++ plot1.R
@@ -1,14 +1,17 @@
-
 library(datasets)
+
+#read the file
+
 data1<-read.csv('household_power_consumption.txt',sep=';',na.strings = "?",strip.white=TRUE,
                 stringsAsFactors=FALSE)
 
+#select the data rows
 data2 <- data1[as.Date(data1$Date,format='%d/%m/%Y') >='2007-02-01' 
                & as.Date(data1$Date,format='%d/%m/%Y') <='2007-02-02'  ,]
 
 data1<-NULL
 
-
+#put the plot in a file
 png(
     "plot1.png",
     width = 480,
--- plot3.R
+++ plot3.R
@@ -1,16 +1,22 @@
+# change the Locale from my native locale to get the abbreviations 'Thu' ,'Fri', and 'Sat' on the graph
 Sys.setlocale("LC_TIME", "English")
+
 library(datasets)
+
+#read the file
 data1<-read.csv('household_power_consumption.txt',sep=';',na.strings = "?",strip.white=TRUE,
                 stringsAsFactors=FALSE)
 
+#select the data rows
 data2 <- data1[as.Date(data1$Date,format='%d/%m/%Y') >='2007-02-01' 
                & as.Date(data1$Date,format='%d/%m/%Y') <='2007-02-02'  ,]
-
+# Create a new column 'DateTime', POSIXct format from combining and character columns 'Date' and 'Time' 
 data2["DateTime"]<-paste(data2$Date,data2$Time,sep=" ")
 
 data2["DateTime"]<- as.POSIXct(data2$DateTime, format='%d/%m/%Y %H:%M')
 data1<-NULL
 
+#put the plot in a file
 png(
     "plot3.png",
     width = 480,
